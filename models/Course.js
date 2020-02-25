abfa8c7136b1321b62b0d6f3bf2f2f293e383fef
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-02-25 09:05:29 +0700

    Course Model & Seeding

---
+++ models/Course.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CourseSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a course title']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description']
+  },
+  weeks: {
+    type: Number,
+    required: [true, 'Please add a number of weeks']
+  },
+  tuition: {
+    type: Number,
+    required: [true, 'Please add a tuition cost']
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, 'Please add a minimum skill'],
+    enum: ['beginner', 'intermediate', 'advanced']
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  },
+  bootcamp: {
+    type: Schema.Types.ObjectId,
+    ref: 'Bootcamp',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
--- seeder.js
+++ seeder.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const colors = require('colors');
 
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,10 +19,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 // Import data
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data Imported...'.green.inverse);
   } catch (error) {
     console.error(error);
@@ -33,6 +39,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data Destroyed...'.red.inverse);
   } catch (error) {
     console.error(error);
