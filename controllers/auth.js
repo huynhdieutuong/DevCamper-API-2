8c3bad989609771515af75521a6c3a2660cfe001
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-03-07 18:28:36 +0700

    Login User

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('../middlewares/async');
 const User = require('../models/User');
 const sendTokenResponse = require('../utils/sendTokenResponse');
+const ErrorResponse = require('../utils/errorResponse');
 
 // @desc    Register user
 // @route   POST  /api/v2/auth/register
@@ -9,6 +10,32 @@ exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, role, password } = req.body;
 
   const user = await User.create({ name, email, role, password });
+
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc    Login user
+// @route   POST  /api/v2/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('password');
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check for password
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
 
   sendTokenResponse(user, 200, res);
 });
