91d3d57d088673d81ad05627cf8fd821565f5882
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-03-24 17:57:37 +0700

    Forgot Password - Reset Password Token Generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,3 +49,28 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     data: req.user
   });
 });
+
+// @desc    Forgot password
+// @route   POST  /api/v2/auth/forgotpassword
+// @access  Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(
+      new ErrorResponse(
+        `There is no user with the email address ${req.body.email}`,
+        404
+      )
+    );
+  }
+
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login, getMe } = require('../controllers/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword
+} = require('../controllers/auth');
 
 const { protect } = require('../middlewares/auth');
 
@@ -10,5 +15,7 @@ router.post('/register', register);
 router.post('/login', login);
 
 router.get('/me', protect, getMe);
+
+router.post('/forgotpassword', forgotPassword);
 
 module.exports = router;
